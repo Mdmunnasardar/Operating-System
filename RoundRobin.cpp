467bc55f6bf329fe8a853436fbd51e58db2347d1
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2026-03-28 14:42:49 +0600

    fixed round robin output

--- RoundRobin.cpp
+++ RoundRobin.cpp
@@ -6,88 +6,89 @@ using namespace std;
 
 struct Process {
     string pid;
-    int at, bt, ct, wt, tat;
+    int at, bt, rt, ct, tat, wt;
 };
 
 int main() {
-    int n;
+    int n, tq;
     cout << "Enter number of processes: ";
     cin >> n;
 
+    cout << "Enter Time Quantum: ";
+    cin >> tq;
+
     vector<Process> p(n);
 
-    // Input
     for (int i = 0; i < n; i++) {
         p[i].pid = "P" + to_string(i + 1);
-        cout << "Arrival Time for " << p[i].pid << ": ";
-        cin >> p[i].at;
-        cout << "Burst Time for " << p[i].pid << ": ";
-        cin >> p[i].bt;
+        cin >> p[i].at >> p[i].bt;
+        p[i].rt = p[i].bt;
     }
 
-    int q;
-    cout << "Enter Time Quantum: ";
-    cin >> q;
+    queue<int> q;
+    vector<int> gantt, timeLine;
 
-    // Sort by arrival time
-    sort(p.begin(), p.end(), [](Process a, Process b) {
-        return a.at < b.at;
+    int time = 0, i = 0, completed = 0;
+
+    vector<int> idx(n);
+    for (int i = 0; i < n; i++) idx[i] = i;
+
+    sort(idx.begin(), idx.end(), [&](int a, int b) {
+        return p[a].at < p[b].at;
     });
 
-    vector<int> rem_bt(n);
-    for (int i = 0; i < n; i++)
-        rem_bt[i] = p[i].bt;
+    q.push(idx[0]);
+    time = p[idx[0]].at;
+    i = 1;
 
-    queue<int> ready;
-    int time = p[0].at;
-    int i = 1;
+    while (!q.empty()) {
+        int id = q.front();
+        q.pop();
 
-    ready.push(0);
+        gantt.push_back(id);
+        timeLine.push_back(time);
 
-    cout << "\nGantt Chart:\n";
-
-    while (!ready.empty()) {
-        int idx = ready.front();
-        ready.pop();
-
-        int exec = min(q, rem_bt[idx]);
-
-        cout << "| " << p[idx].pid << " ";
-
+        int exec = min(tq, p[id].rt);
         time += exec;
-        rem_bt[idx] -= exec;
+        p[id].rt -= exec;
 
-        // Add newly arrived processes
-        while (i < n && p[i].at <= time) {
-            ready.push(i);
+        while (i < n && p[idx[i]].at <= time) {
+            q.push(idx[i]);
             i++;
         }
 
-        // If process not finished, push back
-        if (rem_bt[idx] > 0) {
-            ready.push(idx);
+        if (p[id].rt > 0) {
+            q.push(id);
         } else {
-            p[idx].ct = time;
-            p[idx].tat = p[idx].ct - p[idx].at;
-            p[idx].wt = p[idx].tat - p[idx].bt;
+            p[id].ct = time;
+            completed++;
         }
     }
 
-    cout << "|\n";
+    timeLine.push_back(time);
 
-    float total_wt = 0, total_tat = 0;
-
-    cout << "\nPID\tAT\tBT\tWT\tTAT\n";
-    for (auto proc : p) {
-        cout << proc.pid << "\t" << proc.at << "\t" << proc.bt << "\t"
-             << proc.wt << "\t" << proc.tat << endl;
-
-        total_wt += proc.wt;
-        total_tat += proc.tat;
+    for (int i = 0; i < n; i++) {
+        p[i].tat = p[i].ct - p[i].at;
+        p[i].wt = p[i].tat - p[i].bt;
     }
 
-    cout << "\nAverage WT: " << total_wt / n;
-    cout << "\nAverage TAT: " << total_tat / n << endl;
+    cout << "\nGantt Chart:\n";
+    cout << "-------------------------------------------------\n";
+
+    for (int id : gantt)
+        cout << "| " << p[id].pid << " ";
+
+    cout << "|\n-------------------------------------------------\n";
+
+    for (int t : timeLine)
+        cout << t << "    ";
+
+    cout << "\n\nPID\tAT\tBT\tCT\tTAT\tWT\n";
+
+    for (auto &x : p) {
+        cout << x.pid << "\t" << x.at << "\t" << x.bt << "\t"
+             << x.ct << "\t" << x.tat << "\t" << x.wt << "\n";
+    }
 
     return 0;
 }
